80ff7b39cea8b03a40ec1ca27d19306a014e955c
Kent Hervey <kent.hervey@gmail.com>
2019-05-01 12:32:32 -0500

    Removed days fr Success Header

--- server.py
+++ server.py
@@ -15,19 +15,20 @@ def dateEntry(): #renders the date entry html template
 def success():
     print("beginning of success route")
     today = datetime.now(pytz.timezone('US/Central')) #hardcoded time zone for now
+    dateadjustment=1
     returned_begin_date=request.form['returnedBeginDate']
     returned_begin_date_wTZ=str(returned_begin_date+'--0600')
     returned_begin_date_obj=datetime.strptime(returned_begin_date_wTZ, '%Y-%m-%d-%z')
     print(returned_begin_date_obj)
     #returned_begin_date_obj=datetime.strptime(returned_begin_date + '--1000', '%Y-%m-%d-%z')    
     days_to_returned_begin_date =(returned_begin_date_obj - today).days
-    output_begin_string1 = "Earliest first harvest date:  " + returned_begin_date + "  which is  " + str(days_to_returned_begin_date) + " days from now"
+    output_begin_string1 = "Earliest harvest date:  " + returned_begin_date
     print(output_begin_string1)
 
     returned_end_date=request.form['returnedEndDate']
     returned_end_date_obj=datetime.strptime(str(returned_end_date+'--0600'), '%Y-%m-%d-%z')
-    days_to_returned_end_date =(returned_end_date_obj - today).days
-    output_begin_string2 = "Latest first harvest date:  " + returned_end_date + "  which is  " + str(days_to_returned_end_date) + " days from now"
+    days_to_returned_end_date =(returned_end_date_obj - today).days+dateadjustment #captures edge case crops
+    output_begin_string2 = "Latest harvest date:  " + returned_end_date
 
 
     catalog_result=make_catalog(days_to_returned_begin_date,days_to_returned_end_date)
@@ -36,7 +37,7 @@ def success():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False)
 
 
 
